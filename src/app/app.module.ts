f36d7e6488c628ccb3d0d485b6c92389232a7b40
Aljaž Berglez <aljaz.berglez@agenda.si>
2022-01-30 09:32:50 +0100

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/main/dashboard/dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/main/forms/login/login.component';
+import { RegisterComponent } from './components/main/forms/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, HeaderComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, MainComponent, HeaderComponent, DashboardComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isAuth!: boolean;
+  private subscription: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.subscription = this.authService
+      .authStatusListener()
+      .subscribe((isAuth: boolean) => {
+        this.isAuth = isAuth;
+      });
   }
-
 }
